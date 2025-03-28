8c831d75a1247c6f29397daf06613fb3bea5e76b
Éricles Miller <ericlesmiller15@gmail.com>
2025-03-28 09:59:34 -0300

    feat: create a new controller to send messages

--- queue-direct-exchange-nestjs-usingmicrosservice/apps/api/src/rabbimq/rabbimq.controller.ts
+++ queue-direct-exchange-nestjs-usingmicrosservice/apps/api/src/rabbimq/rabbimq.controller.ts
@@ -7,13 +7,39 @@ import { RabbitMQService } from './rabbimq.service';
 export class RabbiMqController {
   constructor(private readonly rabbitMQService: RabbitMQService) {}
 
-  @Post()
-  async sendMessage(@Body() message: string) {
-    try {
-      const result = await this.rabbitMQService.publishMessage(message);
-      return { success: result, message: 'Message sent to queue' };
-    } catch (error) {
-      throw error;
-    }
+  @Post('queue1-key1')
+  async sendMessageQueue1Key1(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue1key1(message);
+    return { success: result, message: 'Message sent to queue' };
+  }
+
+  @Post('queue1-key2')
+  async sendMessageQueue1Key2(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue1key2(message);
+    return { success: result, message: 'Message sent to queue' };
+  }
+
+  @Post('queue2-key1')
+  async sendMessageQueue2Key1(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue2key1(message);
+    return { success: result, message: 'Message sent to queue' };
+  }
+
+  @Post('queue2-key2')
+  async sendMessageQueue2Key2(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue2key2(message);
+    return { success: result, message: 'Message sent to queue' };
+  }
+
+  @Post('queue3')
+  async sendMessageQueue3(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue3(message);
+    return { success: result, message: 'Message sent to queue' };
+  }
+
+  @Post('queue4')
+  async sendMessageQueue4(@Body() message: string) {
+    const result = await this.rabbitMQService.sendToQueue3(message);
+    return { success: result, message: 'Message sent to queue' };
   }
 }
